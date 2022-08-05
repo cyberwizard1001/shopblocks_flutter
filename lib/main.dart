09c18fe0d075e3f479d213884ff32340f058ff13
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2022-08-06 00:08:13 +0530

    Added landing image and downgraded google_fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shopblocks_flutter/screens/home.dart';
+import 'package:shopblocks_flutter/screens/landing_page.dart';
+import 'package:shopblocks_flutter/screens/login.dart';
 import 'utils/colors.dart' as colors;
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: colors.scaffoldColor,
-        primaryColor: colors.primaryTextColor,
-        textTheme: TextTheme(
-          headline1: GoogleFonts.raleway(
-            color: colors.primaryTextColor,
-          ),
-        ),
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: colors.primaryTextColor,
-          secondary: colors.accentColor,
-        ),
-      ),
-      home: const Home(),
+      home: const LandingPage(),
     ),
   );
 }
--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shopblocks_flutter/utils/colors.dart' as colors;
+import 'package:shopblocks_flutter/utils/custom_sliver_widget.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({Key? key}) : super(key: key);
@@ -7,7 +10,30 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: colors.scaffoldColor,
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          SvgPicture.asset('assets/images/landing-background.svg',fit: BoxFit.fill,),
+          //Image(image: const AssetImage('assets/images/landing_background.png'),fit: BoxFit.fitHeight),
+          CustomSliverView(
+           columnList: [
+              Expanded(child: Container(),flex: 5,),
+              Expanded(child: Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                        Text('SupplyBlocks',style: GoogleFonts.raleway(fontSize: 45,color: colors.primaryTextColor,fontWeight: FontWeight.bold),)
+                    ],
+                  ),
+                ),
+              ),flex: 3,)
+            ],
+          )
+        ],
+      ),
     );
   }
 }
